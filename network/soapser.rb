89e86412c0a83bd484135b1a22dd92b88aa420ff
Ricardo Victor Guiman II <rvguiman@gmail.com>
2014-06-18 17:18:46 +0800

    Create soapser.rb

---
+++ network/soapser.rb
@@ -0,0 +1,26 @@
+require "soap/rpc/standaloneServer"
+
+begin
+  class ServerClass < SOAP::RPC::StandaloneServer
+    def initialize(*args)
+      super(args[0], args[1], args[2], args[3])
+      add_method(self, 'sum', 'a', 'b')
+      add_method(self, 'divide', 'a', 'b')
+    end
+    
+    def sum(a, b)
+      return a.to_i + b.to_i
+    end
+    def divide(a, b)
+      return a.to_i / b.to_i
+    end
+  end
+  server = ServerClass.new("MyServer",
+                        'urn:ruby:mathcalc', 'localhost', 9900)
+  trap('INT') {
+    server.shutdown
+  }
+  server.start
+  rescue => err
+  puts err.message
+end
