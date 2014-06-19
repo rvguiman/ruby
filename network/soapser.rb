8183a38e9f625b6a4d41cfb14082b4588eb18ac0
Ricardo Victor Guiman II <rvguiman@gmail.com>
2014-06-19 10:36:19 +0800

    Update soapser.rb

--- network/soapser.rb
+++ network/soapser.rb
@@ -5,14 +5,14 @@ begin
     def initialize(*args)
       super(args[0], args[1], args[2], args[3])
       add_method(self, 'sum', 'a', 'b')
-      add_method(self, 'divide', 'a', 'b')
+      add_method(self, 'dif', 'a', 'b')
     end
     
     def sum(a, b)
       return a.to_i + b.to_i
     end
-    def divide(a, b)
-      return a.to_i / b.to_i
+    def dif(a, b)
+      return a.to_i - b.to_i
     end
   end
   server = ServerClass.new("MyServer",
